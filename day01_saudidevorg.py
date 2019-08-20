179903defc82b913c298f09dc1a747f7dcaf3d86
Zinab Abdulmanea <zabdulmanea@gmail.com>
2019-08-20 23:21:06 +0300

    Write IF-statement block without using the indentation

--- day01_saudidevorg.py
+++ day01_saudidevorg.py
@@ -5,3 +5,7 @@ print("This is the first lesson of 100 Days of code.")
 # Write a block using the indentation
 if 5 < 10:
     print("Five is smaller than Ten!")
+
+# Write a block without using the indentation
+if 5 < 10:
+print("Five is smaller than Ten!")
