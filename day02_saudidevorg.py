d0a8f5360027898d92fe044ffbe0ac0a2ad0329f
Zinab Abdulmanea <zabdulmanea@gmail.com>
2019-08-21 18:12:28 +0300

    Write multiline comment

--- day02_saudidevorg.py
+++ day02_saudidevorg.py
@@ -1,10 +1,16 @@
 # Python Comments
+print("Day 2, saudidevorg")
 
 # One line comment on a single line
-print("Hello, World!")
+print("A statement after one line comment")
 
-print("Day 2, saudidevorg") # One line comment at the end of a line
+print("A statement on the same line of a one line comment") # One line comment at the end of a line
 
 # print("Don't Execute this code, because it's a comment!")
 
+# Multi-line comments
+# Insert hashtag for each line
+# to insert multiline comment
+# like this
+print("A statement after a multiline comment")
 
