58dde5500a3b4b14ccb490902a2d5c6636cbdb51
Zinab Abdulmanea <zabdulmanea@gmail.com>
2019-08-21 16:54:25 +0300

    Write one line comments

---
+++ day02_saudidevorg.py
@@ -0,0 +1,10 @@
+# Python Comments
+
+# One line comment on a single line
+print("Hello, World!")
+
+print("Day 2, saudidevorg") # One line comment at the end of a line
+
+# print("Don't Execute this code, because it's a comment!")
+
+
