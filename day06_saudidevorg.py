99b41bc77a6b413b89d001cf46c1cae8bb4c72d0
Zinab Abdulmanea <zabdulmanea@gmail.com>
2019-08-25 20:33:00 +0300

    Casting to int, float and str types

---
+++ day06_saudidevorg.py
@@ -0,0 +1,27 @@
+# Python Casting
+
+# How to use int()
+x = int(1)
+y = int(2.976)
+z = int("5")
+
+print("Casting to integer numbers:")
+print(x, y, z)
+
+# How to use float()
+x = float(2)
+y = float(5.986)
+z = float("9")
+w = float("6.73")
+
+print("Casting to float numbers:")
+print(x, y, z, w)
+
+# How to use str()
+x = str("Zinab2019")
+y = str(50)
+z = str("5.97")
+
+print("Casting to strings:")
+print(x, y, z)
+print(type(x), type(y), type(z))
