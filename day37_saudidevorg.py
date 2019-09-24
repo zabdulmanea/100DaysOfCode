e94263ccfaad630d51eb6dbdb7f73e157768d2ae
Zinab Abdulmanea <zabdulmanea@gmail.com>
2019-09-25 01:22:46 +0300

    Arrays 1

---
+++ day37_saudidevorg.py
@@ -0,0 +1,16 @@
+# Python Arrays - part 1
+
+# Create an array containing car names
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+# Access the Elements of an Array
+print(cars[0])
+
+# Change the item value
+# Modify the value of the first array item
+cars[0] = "Toyota"
+print(cars)
+
+# The Length of an Array
+print(len(cars))
