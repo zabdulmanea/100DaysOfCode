f82c453f858bc5000e637d92da029e4761a751ee
Zinab Abdulmanea <zabdulmanea@gmail.com>
2019-09-24 07:22:07 +0300

    Lambda

---
+++ day35_saudidevorg.py
@@ -0,0 +1,27 @@
+# Python Lambda
+# It is a small anonymous function
+# Syntax: "lambda arguments: expression"
+
+# one argument
+x = lambda a : a + 10
+print(x(5))
+
+# def x(a):
+#     return a + 10
+# print(x(5))
+
+# two arguments
+x = lambda a, b : a * b
+print(x(5, 2))
+
+# def x(a, b):
+#     return a * b
+# print(x(5, 2))
+
+# three arguments
+x = lambda a, b, c : a + b + c
+print(x(5,3,7))
+
+# def x(a, b, c):
+#     return a + b + c
+# print(x(5,3,7))
